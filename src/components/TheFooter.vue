44f82a254a4e020ec0cf5e460a487bc6b319d57f
ashley <ashley.li@thinkpower-info.com>
2019-12-07 00:27:16 +0800

    no message

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,54 @@
+﻿<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <div class="row justify-content-around">
+        <div class="col-8 col-md-5">
+          <h5 class="title">詭祕之主</h5>
+          <p class="description">＠烏賊</p>
+        </div>
+        <div class="col-2">
+          <ul class="list-unstyled">
+            <li>
+              Contact Us:
+              <a class="footerlink" href="mailto:liestrella1171@gmail.com">liestrella1171@gmail.com</a>
+            </li>
+            <li>
+              <a class="footerlink" href="/">塔羅會</a>
+            </li>
+            <li>
+              <a class="footerlink" href="/">奧黛莉後援會</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "TheFooter"
+};
+</script>
+
+
+<style scoped>
+.footer {
+  background-color: #B49082;
+  padding-top: 2rem;
+  padding-bottom: 4rem;
+}
+
+.title {
+  color: #fff;
+}
+
+.description {
+  color: #fff;
+}
+
+.footerlink,
+.footerlink:hover {
+  color: #fff;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+﻿import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "@/views/Home.vue";
+
+Vue.use(VueRouter);
+
+// TODO Web Template Studio: Add routes for your new pages here.
+export default new VueRouter({
+  mode: "history",
+  routes: [
+    { path: "/Home", component: Home },
+
+    { path:"/", redirect: "/Home" }
+  ]
+});
