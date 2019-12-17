f1f5fc4d84555c75f510112cc47312dda2373e2e
Ashley <kna8421@gmail.com>
2019-12-17 12:46:00 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
-﻿module.exports = {
-  devServer: {
-    proxy: "http://localhost:3001",
-    port: 3000
-  },
-  outputDir: "build"
-};
+﻿// module.exports = {
+//   devServer: {
+//     proxy: "http://localhost:3001",
+//     port: 3000
+//   },
+//   outputDir: "build"
+// };
+
+module.exports = {
+publicPath:process.env.NODE_ENV === 'prod'
+? '/my-project/'
+: '/'
+
+}
